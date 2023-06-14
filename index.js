aeee7a0ab2d653f1fa78ff69d0cebb5b2810760e
jeliton soares <jelitonn.soarres@gmail.com>
2023-06-14 14:44:40 -0300

    Juntando duas listas ordenadas

--- index.js
+++ index.js
@@ -4,5 +4,30 @@ const { clientesHomensOrdenado, clientesMulheresOrdenado} = require('./ordena');
 
 function juntaListas(lista1, lista2) {
     let listaFinal = [];
+    let i1 = 0;
+    let i2 = 0;
+
+    while(i1 < lista1.length && i2 < lista2.length) {
+        if (lista1[i1].idade < lista2[i2].idade){
+            listaFinal.push(lista1[i1]);
+            i1++
+        } else {
+            listaFinal.push(lista2[i2]);
+            i2++
+        }
+    }
+
+    while(i1 < lista1.length) {
+        listaFinal.push(lista1[i1]);
+        i1++
+    }
+
+    while(i2 < lista2.length) {
+        listaFinal.push(lista2[i2]);
+        i2++
+    }
     
+    return listaFinal
 }
+
+console.log(juntaListas(clientesHomensOrdenado, clientesMulheresOrdenado))
